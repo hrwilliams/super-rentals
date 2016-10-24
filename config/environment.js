4e571c6de3c01a2a4ce9253202841bd9eb14367b
Halle Williams <williams.hr@gmail.com>
2016-10-23 23:16:00 -0700

    initial commit

--- config/environment.js
+++ config/environment.js
@@ -4,17 +4,20 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
-    rootURL: '/',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
+    },
+
+    firebase: {
+      apiKey: 'AIzaSyD8Y90Mk7EIl3JdAyX2uBaLmwXsSTGiHFw',
+      authDomain: 'super-rentals-d9dd8.firebaseapp.com',
+      databaseURL: 'https://super-rentals-d9dd8.firebaseio.com',
+      storageBucket: 'super-rentals-d9dd8.appspot.com',
     },
 
     APP: {
